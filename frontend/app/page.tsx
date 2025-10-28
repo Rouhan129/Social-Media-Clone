f71e3d9cb1be0ecbd61a75bcc3ada8b26b313bc5
Rouhan <rouhan.faisal@hytgenx.com>
2025-10-28 19:48:21 +0500

    Logout corrected

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 // import { redirect } from "next/navigation";
 
 export default function Home() {
@@ -5,10 +7,10 @@ export default function Home() {
   // const token  = typeof window !== "undefined" ? localStorage.getItem("accessToken") : null
   // if (!token) redirect('/login')
   return (
-    <main>
+    <main className="flex flex-col items-center gap-3 mt-5">
       <h1>Welcome</h1>
       <p>You are logged in!</p>
-      {/* <button onClick={() => {localStorage.clear(); window.location.href = '/login'}}>Logout</button> */}
+      <button onClick={() => {localStorage.clear(); window.location.href = '/login'}} className="bg-gray-600 text-white rounded-sm p-4 hover:bg-gray-400 cursor-pointer">Logout</button>
     </main>
   );
 }
